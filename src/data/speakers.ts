19ac908f84f13d17dc2d020a77ca0cccf9c0972a
Marufbek-Mamatqulov <marufbekofficial787@gmail.com>
2025-09-11 15:58:39 +0500

    Add all missing speakers back to the speakers list

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -37,6 +37,106 @@ export const speakers: Speaker[] = [
     }
   },
   {
+    id: 's3',
+    name: { uz: 'Regina Maria Castillo', en: 'Regina Maria Castillo', ru: 'Regina Maria Castillo' },
+    role: {
+      uz: 'UNICEF Ozbekistondagi vakili',
+      en: 'UNICEF Representative in Uzbekistan',
+      ru: 'Predstavitel YUNISEF v Uzbekistane'
+    },
+    photo: '/Spikerlar/Ms. Regina Maria Castillo.png'
+  },
+  {
+    id: 's4',
+    name: { uz: 'Nikolaz Foucaud', en: 'Nikolaz Foucaud', ru: 'Nikolaz Foucaud' },
+    role: {
+      uz: 'Coursera EMEA & APAC boshqaruvchi direktori',
+      en: 'Managing Director Coursera EMEA & APAC',
+      ru: 'Upravlyayushchiy direktor Coursera EMEA & APAC'
+    },
+    photo: '/Spikerlar/Nikolaz Foucaud.png'
+  },
+  {
+    id: 's5',
+    name: { uz: 'Narek Aslikyan', en: 'Narek Aslikyan', ru: 'Narek Aslikyan' },
+    role: {
+      uz: 'Armaniston Code Academy hamkuri',
+      en: 'Co-founder at Armenian Code Academy',
+      ru: 'Souchreditel Armenian Code Academy'
+    },
+    photo: '/Spikerlar/Narek Aslikyan .png'
+  },
+  {
+    id: 's6',
+    name: { uz: 'Naora Zurutuza', en: 'Naora Zurutuza', ru: 'Naora Zurutuza' },
+    role: {
+      uz: 'Giga UNICEF bosh texnologiya direktori',
+      en: 'Chief Technology Officer of Giga UNICEF',
+      ru: 'Tekhnicheskiy direktor Giga UNICEF'
+    },
+    photo: '/Spikerlar/Naora Zurutuza.png'
+  },
+  {
+    id: 's7',
+    name: { uz: 'Boris Bobrovnikov', en: 'Boris Bobrovnikov', ru: 'Boris Bobrovnikov' },
+    role: {
+      uz: '33 yillik tajribaga ega jahon IT eksperti',
+      en: 'Worldwide IT expert, 33 years experience',
+      ru: 'Mirovoy IT ekspert, 33 goda opyta'
+    },
+    photo: '/Spikerlar/Boris Bobrovnikov.png'
+  },
+  {
+    id: 's8',
+    name: { uz: 'Xurshidbek Kurbanov', en: 'Khurshidbek Kurbanov', ru: 'Khurshidbek Kurbanov' },
+    role: {
+      uz: 'Paybek kompaniyasi Engineering Lead',
+      en: 'Engineering Lead at Paybek',
+      ru: 'Engineering Lead v Paybek'
+    },
+    photo: '/Spikerlar/Xurshidbek Kurbanov.png'
+  },
+  {
+    id: 's9',
+    name: { uz: 'Maxmudjon Sodiqov', en: 'Makhmudjon Sodikov', ru: 'Makhmudjon Sodikov' },
+    role: {
+      uz: 'Professor, Central Asian University; Team Manager & Senior ML Engineer (Vention)',
+      en: 'Professor, Central Asian University; Team Manager & Senior ML Engineer (Vention)',
+      ru: 'Professor, Central Asian University; Rukovoditel komandy i Senior ML Engineer (Vention)'
+    },
+    photo: '/Spikerlar/Makhmudjon Sodikov.png'
+  },
+  {
+    id: 's10',
+    name: { uz: 'Saud Abdulwaheed', en: 'Saud Abdulwaheed', ru: 'Saud Abdulwaheed' },
+    role: {
+      uz: 'HSec asoschisi',
+      en: 'Founder of HSec',
+      ru: 'Osnovatel HSec'
+    },
+    photo: '/Spikerlar/Saud Abdulwaheed .png'
+  },
+  {
+    id: 's11',
+    name: { uz: 'Oybek Artikov', en: 'Oybek Artikov', ru: 'Oybek Artikov' },
+    role: {
+      uz: 'Muhammad al-Xorazmiy avlodlari dasturi mentori',
+      en: 'Mentor of The Successors of Muhammad Al-Khwarizmi',
+      ru: 'Nastavnik programmy Preemniki Mukhammada al-Khorezmi'
+    },
+    photo: '/Spikerlar/Oybek Artikov .png'
+  },
+  {
+    id: 's12',
+    name: { uz: 'Azizbek Xabibullaev', en: 'Azizbek Khabibullaev', ru: 'Azizbek Khabibullaev' },
+    role: {
+      uz: 'Front-end dasturchi',
+      en: 'Front End Developer',
+      ru: 'Front End razrabotchik'
+    },
+    photo: '/Spikerlar/Azizbek Khabibullaev.png'
+  },
+  {
     id: 's13',
     name: { uz: 'Vera Dianova', en: 'Vera Dianova', ru: 'Vera Dianova' },
     role: {
@@ -45,5 +145,15 @@ export const speakers: Speaker[] = [
       ru: 'UUM Academy'
     },
     photo: '/Spikerlar/Vera Dianova.png'
+  },
+  {
+    id: 's14',
+    name: { uz: 'Aleksandra Nabieva', en: 'Alexandra Nabieva', ru: 'Alexandra Nabieva' },
+    role: {
+      uz: 'Huawei Seeds for the Future',
+      en: 'Huawei Seeds for the Future',
+      ru: 'Huawei Seeds for the Future'
+    },
+    photo: '/Spikerlar/Alexandra Nabieva.png'
   }
 ];
