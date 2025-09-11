078a2c4443118c8766c41911d17d2a32404e299b
Marufbek-Mamatqulov <marufbekofficial787@gmail.com>
2025-09-11 15:49:52 +0500

    Fix speakers.ts file structure and encoding issues

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,167 +1,49 @@
-export type LocaleText = { u  {
-    id: 's2',
-    name: { uz: 'Ism Familiya', en: 'Name Surname', ru: 'Имя Фамилия' },
-    role: {
-      uz: "O'zbekiston Respublikasi Maktabgacha va maktab taʼlimi vaziri o'rinbosari",
-      en: 'Deputy Minister for Preschool and School Education of Uzbekistan',
-      ru: 'Заместитель министра дошкольного и школьного образования Узбекистана'
-    }
-  }, en: string; ru: string };
+﻿export type LocaleText = { uz: string; en: string; ru: string };
+
 export type Speaker = {
   id: string;
   name: LocaleText;
   role: LocaleText;
-  photo?: string; // optional image path under /public
+  photo?: string;
 };
 
 export const speakers: Speaker[] = [
   {
     id: 's0',
-    name: { uz: 'Rustam Karimjonov', en: 'Rustam Karimjonov', ru: 'Рустам Каримжонов' },
+    name: { uz: 'Rustam Karimjonov', en: 'Rustam Karimjonov', ru: 'Rustam Karimjonov' },
     role: {
-      uz: "O‘zbekiston Respublikasi Raqamli texnologiyalar vaziri o‘rinbosari",
+      uz: 'Ozbekiston Respublikasi Raqamli texnologiyalar vaziri orinbosari',
       en: 'Deputy Minister at the Ministry for the Development of ICT of the Republic of Uzbekistan',
-      ru: 'Заместитель министра по развитию ИКТ Республики Узбекистан'
+      ru: 'Zamestitel ministra po razvitiyu IKT Respubliki Uzbekistan'
     },
-  photo: '/Spikerlar/image.png'
+    photo: '/Spikerlar/image.png'
   },
   {
     id: 's1',
-    name: { uz: 'Ism Familiya', en: 'Name Surname', ru: 'Имя Фамилия' },
+    name: { uz: 'Ism Familiya', en: 'Name Surname', ru: 'Imya Familiya' },
     role: {
-      uz: 'Oliy taʼlim, fan va innovatsiyalar vaziri o‘rinbosari',
+      uz: 'Oliy talim, fan va innovatsiyalar vaziri orinbosari',
       en: 'Deputy Minister of Higher Education, Science and Innovation of Uzbekistan',
-      ru: 'Заместитель министра высшего образования, науки и инноваций Узбекистана'
+      ru: 'Zamestitel ministra vysshego obrazovaniya, nauki i innovatsiy Uzbekistana'
     }
   },
   {
     id: 's2',
-    name: { uz: 'Sardor Radjabov', en: 'Sardor Radjabov', ru: 'Сардор Раджабов' },
+    name: { uz: 'Ism Familiya', en: 'Name Surname', ru: 'Imya Familiya' },
     role: {
-      uz: "O‘zbekiston Respublikasi Maktabgacha va maktab taʼlimi vaziri o‘rinbosari",
+      uz: 'Ozbekiston Respublikasi Maktabgacha va maktab talimi vaziri orinbosari',
       en: 'Deputy Minister for Preschool and School Education of Uzbekistan',
-      ru: 'Заместитель министра дошкольного и школьного образования Узбекистана'
-    },
-    photo: '/Spikerlar/Radjabov Sardor Baxtiyorovich.png'
-  },
-  {
-    id: 's3',
-    name: { uz: 'Regina Maria Castillo', en: 'Regina Maria Castillo', ru: 'Регина Мария Кастильо' },
-    role: {
-      uz: 'UNICEF Oʻzbekistondagi vakili',
-      en: 'UNICEF Representative in Uzbekistan',
-      ru: 'Представитель ЮНИСЕФ в Узбекистане'
-    },
-    photo: '/Spikerlar/Ms. Regina Maria Castillo.png'
-  },
-  {
-    id: 's4',
-    name: { uz: 'Nikolaz Foucaud', en: 'Nikolaz Foucaud', ru: 'Николаз Фука' },
-    role: {
-      uz: 'Coursera EMEA & APAC boshqaruvchi direktori',
-      en: 'Managing Director Coursera EMEA & APAC',
-      ru: 'Управляющий директор Coursera EMEA & APAC'
-    },
-    photo: '/Spikerlar/Nikolaz Foucaud.png'
-  },
-  {
-    id: 's5',
-    name: { uz: 'Narek Aslikyan', en: 'Narek Aslikyan', ru: 'Нарек Асликян' },
-    role: {
-      uz: 'Armaniston Code Academy hamkuri',
-      en: 'Co‑founder at Armenian Code Academy',
-      ru: 'Соучредитель Armenian Code Academy'
-    },
-    photo: '/Spikerlar/Narek Aslikyan .png'
-  },
-  {
-    id: 's6',
-    name: { uz: 'Naora Zurutuza', en: 'Naora Zurutuza', ru: 'Наора Зурутуза' },
-    role: {
-      uz: 'Giga UNICEF bosh texnologiya direktori',
-      en: 'Chief Technology Officer of Giga UNICEF',
-      ru: 'Технический директор Giga UNICEF'
-    },
-    photo: '/Spikerlar/Naora Zurutuza.png'
-  },
-  {
-    id: 's7',
-    name: { uz: 'Boris Bobrovnikov', en: 'Boris Bobrovnikov', ru: 'Борис Бобровников' },
-    role: {
-      uz: '33 yillik tajribaga ega jahon IT eksperti',
-      en: 'Worldwide IT expert, 33 years experience',
-      ru: 'Мировой IT эксперт, 33 года опыта'
-    },
-    photo: '/Spikerlar/Boris Bobrovnikov.png'
-  },
-  {
-    id: 's8',
-    name: { uz: 'Xurshidbek Kurbanov', en: 'Khurshidbek Kurbanov', ru: 'Хуршидбек Курбанов' },
-    role: {
-      uz: 'Paybek kompaniyasi Engineering Lead',
-      en: 'Engineering Lead at Paybek',
-      ru: 'Engineering Lead в Paybek'
-    },
-    photo: '/Spikerlar/Xurshidbek Kurbanov.png'
-  },
-  {
-    id: 's9',
-    name: { uz: 'Maxmudjon Sodiqov', en: 'Makhmudjon Sodikov', ru: 'Махмуджон Содиков' },
-    role: {
-      uz: 'Professor, Central Asian University; Team Manager & Senior ML Engineer (Vention)',
-      en: 'Professor, Central Asian University; Team Manager & Senior ML Engineer (Vention)',
-      ru: 'Профессор, Central Asian University; Руководитель команды и Senior ML Engineer (Vention)'
-    },
-    photo: '/Spikerlar/Makhmudjon Sodikov.png'
-  },
-  {
-    id: 's10',
-    name: { uz: 'Saud Abdulwaheed', en: 'Saud Abdulwaheed', ru: 'Сауд Абдулвахид' },
-    role: {
-      uz: 'HSec asoschisi',
-      en: 'Founder of HSec',
-      ru: 'Основатель HSec'
-    },
-    photo: '/Spikerlar/Saud Abdulwaheed .png'
-  },
-  {
-    id: 's11',
-    name: { uz: 'Oybek Artikov', en: 'Oybek Artikov', ru: 'Ойбек Артиков' },
-    role: {
-      uz: "Muhammad al-Xorazmiy avlodlari dasturi mentori",
-      en: 'Mentor of "The Successors of Muhammad Al-Khwarizmi"',
-      ru: 'Наставник программы "Преемники Мухаммада аль-Хорезми"'
-    },
-    photo: '/Spikerlar/Oybek Artikov .png'
-  },
-  {
-    id: 's12',
-    name: { uz: 'Azizbek Xabibullaev', en: 'Azizbek Khabibullaev', ru: 'Азизбек Хабибуллаев' },
-    role: {
-      uz: 'Front-end dasturchi',
-      en: 'Front End Developer',
-      ru: 'Front End разработчик'
-    },
-    photo: '/Spikerlar/Azizbek Khabibullaev.png'
+      ru: 'Zamestitel ministra doshkolnogo i shkolnogo obrazovaniya Uzbekistana'
+    }
   },
   {
     id: 's13',
-    name: { uz: 'Vera Dianova', en: 'Vera Dianova', ru: 'Вера Дианова' },
+    name: { uz: 'Vera Dianova', en: 'Vera Dianova', ru: 'Vera Dianova' },
     role: {
       uz: 'UUM Academy',
       en: 'UUM Academy',
       ru: 'UUM Academy'
     },
     photo: '/Spikerlar/Vera Dianova.png'
-  },
-  {
-    id: 's14',
-    name: { uz: 'Aleksandra Nabieva', en: 'Alexandra Nabieva', ru: 'Александра Набиева' },
-    role: {
-      uz: 'Huawei "Seeds for the Future"',
-      en: 'Huawei "Seeds for the Future"',
-      ru: 'Huawei «Seeds for the Future»'
-    },
-    photo: '/Spikerlar/Alexandra Nabieva.png'
   }
 ];
